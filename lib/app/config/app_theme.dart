e082a39ee65b2ca31e61ae2b9e8ff5e5ebd869a6
HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>
2025-05-06 18:40:51 +0900

    feat(#4): MainAppBar UI 구현
    Signed-off-by: HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>

--- lib/app/config/app_theme.dart
+++ lib/app/config/app_theme.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:grimity/app/config/app_typeface.dart';
 
 class AppTheme {
@@ -12,6 +13,11 @@ class AppTheme {
       elevation: 0,
       centerTitle: false,
       titleTextStyle: AppTypeface.subTitle3,
+      toolbarHeight: kToolbarHeight.height,
+      titleSpacing: 16.w,
+      actionsPadding: EdgeInsets.only(right: 16.w),
     ),
   );
+
+  static Size get kToolbarHeight => Size.fromHeight(48.h);
 }
