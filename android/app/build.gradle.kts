94cbc909deac372c6ee6a6e747e22c2de6324db5
wonyong <wanyong0919@naver.com>
2025-07-15 18:21:14 +0900

    chore(#39): Android Flavor 설정
    - Android dev, prod flavor 추가
    - flavor에 따라 이름 표시 Grimity(dev), Grimity

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application")
     // START: FlutterFire Configuration
-    id("com.google.gms.google-services")
+    // id("com.google.gms.google-services")
     // END: FlutterFire Configuration
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
@@ -11,7 +11,7 @@ plugins {
 android {
     namespace = "com.example.grimity"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -38,6 +38,19 @@ android {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
+        }
+
+        flavorDimensions += "build-type"
+        productFlavors {
+            create("dev") {
+                dimension = "build-type"
+                resValue(type = "string", name = "app_name", value = "Grimity(dev)")
+                applicationIdSuffix = ".dev"
+            }
+            create("prod") {
+                dimension = "build-type"
+                resValue(type = "string", name = "app_name", value = "Grimity")
+            }
         }
     }
 }
