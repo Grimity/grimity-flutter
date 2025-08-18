50faafa5f0f559c1efe96a8f1be3ef50952b7472
wonyong <wanyong0919@naver.com>
2025-08-18 15:54:08 +0900

    feat(#49): 댓글 관련 부분 공통 부분 추출
    - 게시글, 피드 공통으로 댓글 사용할 수 있도록 처리

---
+++ lib/presentation/comment/enum/comment_type.dart
@@ -0,0 +1,52 @@
+import 'package:grimity/app/base/use_case.dart';
+import 'package:grimity/domain/usecase/feed_comments_usecases.dart';
+import 'package:grimity/domain/usecase/post_comments_usecases.dart';
+
+enum CommentType { feed, post }
+
+extension CommentTypeX on CommentType {
+  UseCase get createCommentUseCase {
+    switch (this) {
+      case CommentType.feed:
+        return createFeedCommentUseCase;
+      case CommentType.post:
+        return createPostCommentUseCase;
+    }
+  }
+
+  UseCase get getCommentsUseCase {
+    switch (this) {
+      case CommentType.feed:
+        return getFeedCommentsUseCase;
+      case CommentType.post:
+        return getPostCommentsUseCase;
+    }
+  }
+
+  UseCase get deleteCommentsUseCase {
+    switch (this) {
+      case CommentType.feed:
+        return deleteFeedCommentUseCase;
+      case CommentType.post:
+        return deletePostCommentUseCase;
+    }
+  }
+
+  UseCase get likeCommentUseCase {
+    switch (this) {
+      case CommentType.feed:
+        return likeFeedCommentUseCase;
+      case CommentType.post:
+        return likePostCommentUseCase;
+    }
+  }
+
+  UseCase get unlikeCommentUseCase {
+    switch (this) {
+      case CommentType.feed:
+        return unlikeFeedCommentUseCase;
+      case CommentType.post:
+        return unlikePostCommentUseCase;
+    }
+  }
+}
