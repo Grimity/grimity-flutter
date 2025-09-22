e9d277b8c8936fcc91777862c8933115111f99a3
wonyong <wanyong0919@naver.com>
2025-09-22 11:37:29 +0900

    feat(#59): 새로고침 기능 구현
    - 공통 GrimityRefreshIndicator 위젯 구현
    - 홈 화면에 새로고침 기능 적용

--- lib/presentation/home/home_view.dart
+++ lib/presentation/home/home_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:gap/gap.dart';
+import 'package:grimity/presentation/common/widget/grimity_refresh_indicator.dart';
 import 'package:grimity/presentation/home/hook/use_infinite_scroll_hook.dart';
 import 'package:grimity/presentation/home/provider/home_data_provider.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -31,20 +32,31 @@ class HomeView extends HookConsumerWidget {
       loadFunction: () async => await ref.read(latestFeedDataProvider.notifier).loadMore(),
     );
 
-    return CustomScrollView(
-      controller: scrollController,
-      slivers: [
-        homeAppBar,
-        SliverToBoxAdapter(child: Gap(16)),
-        SliverToBoxAdapter(child: noticeView),
-        SliverToBoxAdapter(child: Gap(24)),
-        SliverToBoxAdapter(child: feedRankingView),
-        SliverToBoxAdapter(child: Gap(50)),
-        SliverToBoxAdapter(child: latestPostView),
-        SliverToBoxAdapter(child: Gap(50)),
-        SliverToBoxAdapter(child: latestFeedView),
-        SliverToBoxAdapter(child: Gap(16)),
-      ],
+    return NestedScrollView(
+      headerSliverBuilder: (context, innerBoxIsScrolled) => [homeAppBar],
+      body: GrimityRefreshIndicator(
+        onRefresh: () async {
+          await Future.wait([
+            ref.refresh(feedRankingDataProvider.future),
+            ref.refresh(latestPostDataProvider.future),
+            ref.refresh(latestFeedDataProvider.future),
+          ]);
+        },
+        child: CustomScrollView(
+          controller: scrollController,
+          slivers: [
+            SliverToBoxAdapter(child: Gap(16)),
+            SliverToBoxAdapter(child: noticeView),
+            SliverToBoxAdapter(child: Gap(24)),
+            SliverToBoxAdapter(child: feedRankingView),
+            SliverToBoxAdapter(child: Gap(50)),
+            SliverToBoxAdapter(child: latestPostView),
+            SliverToBoxAdapter(child: Gap(50)),
+            SliverToBoxAdapter(child: latestFeedView),
+            SliverToBoxAdapter(child: Gap(16)),
+          ],
+        ),
+      ),
     );
   }
 }
