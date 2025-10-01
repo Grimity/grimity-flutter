59721174638237ba9654b9556f92ead4ad1b43da
wonyong <wanyong0919@naver.com>
2025-10-01 17:47:18 +0900

    feat(#65): 알림 화면 구현
    - View, Page 코드 구조 수정

--- lib/presentation/notification/notification_view.dart
+++ lib/presentation/notification/notification_view.dart
@@ -1,31 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:grimity/presentation/common/widget/grimity_circular_progress_indicator.dart';
-import 'package:grimity/presentation/notification/provider/notification_data_provider.dart';
-import 'package:grimity/presentation/notification/view/notification_body_view.dart';
-import 'package:grimity/presentation/notification/view/notification_empty_view.dart';
 
 class NotificationView extends ConsumerWidget {
-  const NotificationView({super.key, required this.notificationAppBar});
+  const NotificationView({super.key, required this.notificationAppBar, required this.notificationBody});
 
   final PreferredSizeWidget notificationAppBar;
+  final Widget notificationBody;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final notificationAsync = ref.watch(notificationDataProvider);
-
-    return Scaffold(
-      appBar: notificationAppBar,
-      body: notificationAsync.when(
-        data:
-            (notifications) =>
-                notifications.isEmpty ? NotificationEmptyView() : NotificationBodyView(notifications: notifications),
-        error: (error, stackTrace) {
-          /// TODO Error 표시
-          return Text('error');
-        },
-        loading: () => GrimityCircularProgressIndicator(),
-      ),
-    );
+    return Scaffold(appBar: notificationAppBar, body: notificationBody);
   }
 }
