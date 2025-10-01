910bfa7d417dbd0d1a40d3553b5fe1fb536094bf
wonyong <wanyong0919@naver.com>
2025-10-02 00:13:27 +0900

    refactor: rename /aws to /images
    - 기존 /aws 삭제 처리

--- lib/domain/usecase/aws_usecases.dart
+++
@@ -1,9 +0,0 @@
-import 'package:grimity/app/di/di_setup.dart';
-import 'package:grimity/domain/usecase/aws/get_presigned_url_usecase.dart';
-import 'package:grimity/domain/usecase/aws/upload_image_usecase.dart';
-
-final getPresignedUrlUseCase = getIt<GetPresignedUrlUseCase>();
-final getPresignedUrlsUseCase = getIt<GetPresignedUrlsUseCase>();
-
-final uploadImageUseCase = getIt<UploadImageUseCase>();
-final uploadImagesUseCase = getIt<UploadImagesUseCase>();
