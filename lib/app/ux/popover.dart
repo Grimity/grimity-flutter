759357ded590917af4e49b9d793cdac1c78dd3f1
Dev Ttangkong <ttankkeo112@gmail.com>
2025-10-20 19:14:25 +0900

    답장 보내기 기능 구현

---
+++ lib/app/ux/popover.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/widgets.dart';
+
+/// 주어진 [Popover] 인스턴스에 따라 해당 오버레이에 표시할 위젯을 생성합니다.
+typedef PopoverBuilder = Widget Function(Popover popover);
+
+/// 지정된 위젯을 기준으로 오버레이 형태의 팝업을 보여주는 기능을 제공합니다.
+/// 또한 화면 전체 영역을 탭하면 오버레이가 자동으로 닫히도록 설계되었습니다.
+class Popover {
+  Popover({
+    required this.targetLink,
+    this.targetAnchor = Alignment.topLeft,
+    this.followerAnchor = Alignment.topLeft,
+    required this.builder,
+  });
+
+  final LayerLink targetLink;
+  final Alignment targetAnchor;
+  final Alignment followerAnchor;
+  final PopoverBuilder builder;
+
+  OverlayEntry? _entry;
+
+  void show(BuildContext context) {
+    _entry = OverlayEntry(
+      builder: (context) {
+        return Stack(
+          children: [
+            Positioned.fill(
+              child: GestureDetector(
+                behavior: HitTestBehavior.translucent,
+                onTap: hide,
+              ),
+            ),
+            CompositedTransformFollower(
+              link: targetLink,
+              targetAnchor: targetAnchor,
+              followerAnchor: followerAnchor,
+              showWhenUnlinked: false,
+              child: builder(this),
+            ),
+          ],
+        );
+      },
+    );
+
+    Overlay.of(context, rootOverlay: true).insert(_entry!);
+  }
+
+  void hide() {
+    _entry?.remove();
+    _entry = null;
+  }
+}
---
+++ lib/presentation/chat_message/view/chat_message_popover_menu.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:grimity/app/config/app_color.dart';
+import 'package:grimity/app/ux/popover.dart';
+import 'package:grimity/gen/assets.gen.dart';
+import 'package:grimity/presentation/chat_message/provider/chat_message_provider.dart';
+
+class ChatMessagePopoverMenu extends ConsumerWidget {
+  const ChatMessagePopoverMenu({
+    super.key,
+    required this.chatId,
+    required this.popover,
+    required this.message,
+  });
+
+  final String chatId;
+  final Popover popover;
+  final ChatMessage message;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final provider = ref.read(chatMessageProviderProvider(chatId: chatId).notifier);
+
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      spacing: 6,
+      children: [
+        createButtonWidget(
+          icon: Assets.icons.chatMessage.heart,
+          onTap: () {
+            popover.hide();
+          }
+        ),
+        createButtonWidget(
+          icon: Assets.icons.chatMessage.deliver,
+          onTap: () {
+            provider.setInputReply(message);
+            popover.hide();
+          }
+        ),
+      ],
+    );
+  }
+
+  Widget createButtonWidget({
+    required SvgGenImage icon,
+    required VoidCallback onTap,
+  }) {
+    return GestureDetector(
+      behavior: HitTestBehavior.opaque,
+      onTap: onTap,
+      child: Container(
+        padding: EdgeInsets.all(8),
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          border: Border.all(color: AppColor.gray400),
+          color: AppColor.gray00,
+        ),
+        child: icon.svg(width: 18, height: 18, color: AppColor.gray800),
+      ),
+    );
+  }
+}
