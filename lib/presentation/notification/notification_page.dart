4ae5109cb264b4ca3cce9ac504e90e4234711d24
wonyong <wanyong0919@naver.com>
2025-10-22 15:48:16 +0900

    feat: 알림 화면 재시도 기능 추가
    - 로딩 시 스켈레톤 효과 추가

--- lib/presentation/notification/notification_page.dart
+++ lib/presentation/notification/notification_page.dart
@@ -1,11 +1,12 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' hide Notification;
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:grimity/presentation/common/widget/grimity_circular_progress_indicator.dart';
+import 'package:grimity/domain/entity/notification.dart';
 import 'package:grimity/presentation/common/widget/grimity_state_view.dart';
 import 'package:grimity/presentation/notification/notification_view.dart';
 import 'package:grimity/presentation/notification/provider/notification_data_provider.dart';
 import 'package:grimity/presentation/notification/view/notification_body_view.dart';
 import 'package:grimity/presentation/notification/widget/notification_app_bar.dart';
+import 'package:skeletonizer/skeletonizer.dart';
 
 class NotificationPage extends ConsumerWidget {
   const NotificationPage({super.key});
@@ -16,7 +17,7 @@ class NotificationPage extends ConsumerWidget {
 
     return NotificationView(
       notificationAppBar: NotificationAppBar(),
-      notificationBody: notificationAsync.maybeWhen(
+      notificationBody: notificationAsync.when(
         data:
             (notifications) =>
                 notifications.isEmpty
@@ -25,7 +26,8 @@ class NotificationPage extends ConsumerWidget {
                       subTitle: '내 글의 댓글와 좋아요, 다른 작가의 활동 등\n새로운 소식을 알려드려요',
                     )
                     : NotificationBodyView(notifications: notifications),
-        orElse: () => GrimityCircularProgressIndicator(),
+        loading: () => Skeletonizer(child: NotificationBodyView(notifications: Notification.emptyList)),
+        error: (e, s) => GrimityStateView.error(onTap: () => ref.invalidate(notificationDataProvider)),
       ),
     );
   }
