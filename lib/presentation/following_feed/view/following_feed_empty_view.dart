dfa68835362033c67e6cff937fc2865e6be6b529
wonyong <wanyong0919@naver.com>
2025-10-22 21:32:07 +0900

    feat: 팔로잉 추천 작가 재시도 기능 추가

--- lib/presentation/following_feed/view/following_feed_empty_view.dart
+++ lib/presentation/following_feed/view/following_feed_empty_view.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:grimity/presentation/common/provider/author_with_feeds_provider.dart';
 import 'package:grimity/presentation/common/provider/user_auth_provider.dart';
 import 'package:grimity/presentation/common/widget/grimity_state_view.dart';
 import 'package:grimity/presentation/following_feed/view/recommend_author_view.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:skeletonizer/skeletonizer.dart';
 
 class FollowingFeedEmptyView extends ConsumerWidget {
   const FollowingFeedEmptyView({super.key});
@@ -12,20 +10,13 @@ class FollowingFeedEmptyView extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final user = ref.watch(userAuthProvider);
-    final authorWithFeedsAsync = ref.watch(authorWithFeedsDataProvider);
 
     return Column(
       children: [
         user?.followingCount == 0
             ? GrimityStateView.user(title: '아직 팔로우하는 작가가 없어요', subTitle: '관심 있는 작가를 팔로우하고\n새로운 작품 소식을 받아보세요.')
             : GrimityStateView.resultNull(title: '아직 올라온 그림이 없어요.', subTitle: '관심 있는 작가를 팔로우하고\n새로운 작품 소식을 받아보세요.'),
-        authorWithFeedsAsync.maybeWhen(
-          data: (authorWithFeedsList) => FollowingFeedRecommendAuthorListView(authorWithFeedsList: authorWithFeedsList),
-          orElse:
-              () => Skeletonizer(
-                child: FollowingFeedRecommendAuthorListView(authorWithFeedsList: AuthorWithFeeds.emptyList),
-              ),
-        ),
+        FollowingFeedRecommendAuthorListView(),
       ],
     );
   }
--- lib/presentation/following_feed/view/recommend_author_view.dart
+++ lib/presentation/following_feed/view/recommend_author_view.dart
@@ -4,15 +4,17 @@ import 'package:gap/gap.dart';
 import 'package:grimity/app/config/app_color.dart';
 import 'package:grimity/app/config/app_typeface.dart';
 import 'package:grimity/presentation/common/provider/author_with_feeds_provider.dart';
+import 'package:grimity/presentation/common/widget/grimity_state_view.dart';
 import 'package:grimity/presentation/common/widget/user_card/grimity_author_with_feeds_card.dart';
+import 'package:skeletonizer/skeletonizer.dart';
 
 class FollowingFeedRecommendAuthorListView extends ConsumerWidget {
-  const FollowingFeedRecommendAuthorListView({super.key, required this.authorWithFeedsList});
-
-  final List<AuthorWithFeeds> authorWithFeedsList;
+  const FollowingFeedRecommendAuthorListView({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final authorWithFeedsAsync = ref.watch(authorWithFeedsDataProvider);
+
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 16),
       child: Column(
@@ -20,29 +22,45 @@ class FollowingFeedRecommendAuthorListView extends ConsumerWidget {
         spacing: 16,
         children: [
           Text('추천 작가', style: AppTypeface.subTitle1.copyWith(color: AppColor.gray800)),
-          ListView.separated(
-            padding: EdgeInsets.zero,
-            itemCount: authorWithFeedsList.length,
-            shrinkWrap: true,
-            physics: NeverScrollableScrollPhysics(),
-            itemBuilder: (context, index) {
-              final authorWithFeeds = authorWithFeedsList[index];
-
-              return GrimityAuthorWithFeedsCard(
-                authorWithFeeds: authorWithFeeds,
-                onFollowTab:
-                    () => ref
-                        .read(authorWithFeedsDataProvider.notifier)
-                        .toggleFollow(
-                          id: authorWithFeeds.user.id,
-                          follow: authorWithFeeds.user.isFollowing == false ? true : false,
-                        ),
-              );
-            },
-            separatorBuilder: (context, index) => Gap(24),
+          authorWithFeedsAsync.when(
+            data: (authorWithFeedsList) => _AuthorWithFeedsListView(authorWithFeedsList: authorWithFeedsList),
+            loading:
+                () => Skeletonizer(child: _AuthorWithFeedsListView(authorWithFeedsList: AuthorWithFeeds.emptyList)),
+            error: (e, s) => GrimityStateView.error(onTap: () => ref.invalidate(authorWithFeedsDataProvider)),
           ),
         ],
       ),
+    );
+  }
+}
+
+class _AuthorWithFeedsListView extends ConsumerWidget {
+  const _AuthorWithFeedsListView({required this.authorWithFeedsList});
+
+  final List<AuthorWithFeeds> authorWithFeedsList;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return ListView.separated(
+      padding: EdgeInsets.zero,
+      itemCount: authorWithFeedsList.length,
+      shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(),
+      itemBuilder: (context, index) {
+        final authorWithFeeds = authorWithFeedsList[index];
+
+        return GrimityAuthorWithFeedsCard(
+          authorWithFeeds: authorWithFeeds,
+          onFollowTab:
+              () => ref
+                  .read(authorWithFeedsDataProvider.notifier)
+                  .toggleFollow(
+                    id: authorWithFeeds.user.id,
+                    follow: authorWithFeeds.user.isFollowing == false ? true : false,
+                  ),
+        );
+      },
+      separatorBuilder: (context, index) => Gap(24),
     );
   }
 }
