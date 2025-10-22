6ba19a68671bee669e8cc05e3d60e620e0f67436
wonyong <wanyong0919@naver.com>
2025-10-22 16:01:29 +0900

    feat: 스피너 형태의 로딩 인디케이터 추가

---
+++ lib/presentation/common/widget/grimity_loading_indicator.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:grimity/gen/assets.gen.dart';
+import 'package:lottie/lottie.dart';
+
+/// Grimity 공통 로딩 인디케이터 위젯.
+///
+/// Skeletonizer로 처리하기 애매한 구간에서
+/// 간단한 로딩 상태(Spinner 형태)를 표시할 때 사용합니다.
+class GrimityLoadingIndicator extends StatelessWidget {
+  const GrimityLoadingIndicator({
+    super.key,
+    this.useCenter = true,
+    this.padding = const EdgeInsets.symmetric(vertical: 80),
+  });
+
+  final bool useCenter;
+  final EdgeInsetsGeometry padding;
+
+  @override
+  Widget build(BuildContext context) {
+    final indicator = Padding(padding: padding, child: Lottie.asset(Assets.lottie.refresh, width: 24, height: 24));
+
+    return useCenter ? Center(child: indicator) : indicator;
+  }
+}
