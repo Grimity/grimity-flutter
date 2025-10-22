067345e05a8652182957796e86fb7b165af9288b
wonyong <wanyong0919@naver.com>
2025-10-22 14:14:27 +0900

    feat: 게시판 화면 재시도 기능 추가

--- lib/presentation/board/tabs/view/board_view.dart
+++ lib/presentation/board/tabs/view/board_view.dart
@@ -6,6 +6,7 @@ import 'package:grimity/presentation/board/tabs/provider/board_post_data_provide
 import 'package:grimity/presentation/board/tabs/view/board_list_view.dart';
 import 'package:grimity/presentation/board/tabs/widget/board_tab_bar.dart';
 import 'package:grimity/presentation/common/widget/grimity_refresh_indicator.dart';
+import 'package:grimity/presentation/common/widget/grimity_state_view.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:skeletonizer/skeletonizer.dart';
 
@@ -37,33 +38,34 @@ class BoardView extends HookConsumerWidget {
         controller: tabController,
         physics: NeverScrollableScrollPhysics(),
         children:
-            tabList.map((type) {
-              final postAsync = ref.watch(boardPostDataProvider(type));
+        tabList.map((type) {
+          final postAsync = ref.watch(boardPostDataProvider(type));
 
-              return postAsync.maybeWhen(
-                data:
-                    (posts) => GrimityRefreshIndicator(
-                      onRefresh: () async {
-                        await Future.wait([ref.refresh(boardPostDataProvider(type).future)]);
-                      },
-                      child: BoardListView(
-                        posts: posts.posts,
-                        totalCount: posts.totalCount ?? 0,
-                        type: type,
-                        scrollController: scrollController,
-                      ),
-                    ),
-                orElse:
-                    () => Skeletonizer(
-                      child: BoardListView(
-                        posts: Post.emptyList,
-                        totalCount: 0,
-                        type: type,
-                        scrollController: scrollController,
-                      ),
-                    ),
-              );
-            }).toList(),
+          return postAsync.when(
+            data:
+                (posts) => GrimityRefreshIndicator(
+              onRefresh: () async {
+                await Future.wait([ref.refresh(boardPostDataProvider(type).future)]);
+              },
+              child: BoardListView(
+                posts: posts.posts,
+                totalCount: posts.totalCount ?? 0,
+                type: type,
+                scrollController: scrollController,
+              ),
+            ),
+            loading:
+                () => Skeletonizer(
+              child: BoardListView(
+                posts: Post.emptyList,
+                totalCount: 0,
+                type: type,
+                scrollController: scrollController,
+              ),
+            ),
+            error: (e, s) => GrimityStateView.error(onTap: () => ref.invalidate(boardPostDataProvider(type))),
+          );
+        }).toList(),
       ),
     );
   }
