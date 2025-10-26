204858ba73be2bf3460939b2637afed47568a482
Dev Ttangkong <ttankkeo112@gmail.com>
2025-10-26 16:30:25 +0900

    푸시 알림 기초 구현

--- lib/app/entrypoints/main_dev.dart
+++ lib/app/entrypoints/main_dev.dart
@@ -1,10 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/widgets.dart';
 import 'package:grimity/app/environment/environment_enum.dart';
 import 'package:grimity/app/environment/flavor.dart';
+import 'package:grimity/firebase_options_dev.dart';
 import 'package:grimity/presentation/app.dart';
 
 /// dev
 void main() async {
   Flavor.initialize(Env.dev);
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   return runFlavoredApp();
 }
