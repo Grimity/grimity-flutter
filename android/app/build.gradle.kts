819bb287f43a4b75bf7147029ac49d567d202b2e
Dev Ttangkong <ttankkeo112@gmail.com>
2025-10-27 19:08:02 +0900

    포그라운드에서 푸시 알림 표시하도록 업데이트

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -27,6 +27,7 @@ android {
     ndkVersion = "27.0.12077973"
 
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
@@ -39,7 +40,7 @@ android {
         applicationId = "com.grimity.flutter"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 23
+        minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
@@ -82,4 +83,8 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.5")
 }
