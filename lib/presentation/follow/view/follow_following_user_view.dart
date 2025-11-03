631764f3e9767e65a6ec1b27b66345fcae2a54df
wonyong <wanyong0919@naver.com>
2025-11-03 16:55:13 +0900

    feat: 팔로잉, 팔로워 무한 스크롤 수정

--- lib/presentation/follow/view/follow_following_user_view.dart
+++ lib/presentation/follow/view/follow_following_user_view.dart
@@ -3,11 +3,11 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:gap/gap.dart';
 import 'package:grimity/domain/entity/user.dart';
+import 'package:grimity/presentation/common/widget/grimity_infinite_scroll_pagination.dart';
 import 'package:grimity/presentation/follow/widget/follow_user_tile.dart';
 import 'package:grimity/presentation/common/widget/grimity_state_view.dart';
 import 'package:grimity/presentation/follow/enum/follow_enum_tab_type.dart';
 import 'package:grimity/presentation/follow/provider/follow_following_data_provider.dart';
-import 'package:grimity/presentation/home/hook/use_infinite_scroll_hook.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:skeletonizer/skeletonizer.dart';
 
@@ -24,11 +24,20 @@ class FollowingUserView extends HookConsumerWidget {
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 12.h),
       child: users.when(
-        data:
-            (data) =>
-                data.users.isEmpty
-                    ? GrimityStateView.user(subTitle: FollowTabType.following.emptyMessage)
-                    : _FollowingUserListView(users: data.users),
+        data: (data) {
+          final users = data.users;
+
+          if (users.isEmpty) {
+            return GrimityStateView.user(subTitle: FollowTabType.following.emptyMessage);
+          }
+
+          return GrimityInfiniteScrollPagination(
+            isEnabled: data.nextCursor != null,
+            onLoadMore: ref.read(followingDataProvider.notifier).loadMore,
+            child: _FollowingUserListView(users: data.users),
+          );
+        },
+
         loading: () => Skeletonizer(child: _FollowingUserListView(users: User.emptyList)),
         error: (e, s) => GrimityStateView.error(onTap: () => ref.invalidate(followingDataProvider)),
       ),
@@ -36,23 +45,14 @@ class FollowingUserView extends HookConsumerWidget {
   }
 }
 
-class _FollowingUserListView extends HookConsumerWidget {
+class _FollowingUserListView extends ConsumerWidget {
   const _FollowingUserListView({required this.users});
 
   final List<User> users;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final scrollController = useScrollController();
-
-    useInfiniteScrollHook(
-      ref: ref,
-      scrollController: scrollController,
-      loadFunction: () async => await ref.read(followingDataProvider.notifier).loadMore(),
-    );
-
     return ListView.separated(
-      controller: scrollController,
       itemBuilder: (context, index) {
         final user = users[index];
         return FollowUserTile.following(
