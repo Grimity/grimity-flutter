733620e1ed0aa5f24e5fa52e949d249cb15f97f9
wonyong <wanyong0919@naver.com>
2025-11-05 15:02:56 +0900

    fix: 추천 그림 LayoutGird 제거

--- lib/presentation/feed_detail/view/feed_recommend_feed_view.dart
+++ lib/presentation/feed_detail/view/feed_recommend_feed_view.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_layout_grid/flutter_layout_grid.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gap/gap.dart';
 import 'package:grimity/app/config/app_typeface.dart';
 import 'package:grimity/domain/entity/feed.dart';
-import 'package:grimity/presentation/common/widget/grimity_image_feed.dart';
+import 'package:grimity/presentation/common/widget/grimity_feed_grid.dart';
 import 'package:grimity/presentation/common/widget/grimity_state_view.dart';
 import 'package:grimity/presentation/feed_detail/provider/feed_recommend_feed_data_provider.dart';
 import 'package:skeletonizer/skeletonizer.dart';
@@ -41,23 +40,12 @@ class _RecommendFeedListView extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final int rowCount = (feeds.length / 2).ceil();
-
-    return LayoutGrid(
-      columnSizes: [1.fr, 1.fr],
-      rowSizes: List.generate(rowCount, (_) => auto),
-      rowGap: 20,
-      columnGap: 12,
-      children: [
-        for (var feed in feeds)
-          GrimityImageFeed(
-            feed: feed,
-            onToggleLike:
-                () => ref
-                    .read(feedRecommendFeedDataProvider.notifier)
-                    .toggleLike(feedId: feed.id, like: feed.isLike == true ? false : true),
-          ),
-      ],
+    return GrimityFeedGrid(
+      feeds: feeds,
+      onToggleLike:
+          (feed) => ref
+              .read(feedRecommendFeedDataProvider.notifier)
+              .toggleLike(feedId: feed.id, like: feed.isLike == true ? false : true),
     );
   }
 }
