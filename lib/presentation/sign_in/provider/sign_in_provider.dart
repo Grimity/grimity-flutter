d1a9fe37af8bb47af54fa735810949d8085855bf
wonyong <wanyong0919@naver.com>
2025-11-08 11:05:18 +0900

    feat: 카카오 로그인 비동기 호출 후 ref가 dispose되어 로그인 시도를 두번해야하는 문제 수정
    - 비동기 로그인 이후 위젯이 dispose될 수 있는 문제를 고려하여 라우터 참조
    - 로그인 실패 시 Crashlytics로 오류를 전송하도록 수정

--- lib/presentation/sign_in/provider/sign_in_provider.dart
+++ lib/presentation/sign_in/provider/sign_in_provider.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:go_router/go_router.dart';
 import 'package:grimity/app/config/app_router.dart';
 import 'package:grimity/app/enum/login_provider.enum.dart';
 import 'package:grimity/app/service/toast_service.dart';
@@ -14,24 +16,32 @@ class SignIn extends _$SignIn {
   void build() {}
 
   /// 로그인 결과에 따른 라우팅
-  void _route(WidgetRef ref) {
+  void _route(GoRouter router) {
     if (ref.read(userAuthProvider) != null) {
-      HomeRoute().go(ref.context);
+      router.go(HomeRoute.path);
     } else {
-      SignUpRoute().push(ref.context);
+      router.push(SignUpRoute.path);
     }
   }
 
   /// 로그인
-  Future<void> login(WidgetRef ref, LoginProvider provider) async {
+  Future<void> login(WidgetRef widgetRef, LoginProvider provider) async {
     try {
+      // 비동기 통신 이후 widgetRef가 dispose 될 수 있어 라우터 참조
+      final router = AppRouter.router(widgetRef);
+
+      // login 시도
       await ref.read(userAuthProvider.notifier).login(provider);
 
       // 로그인 이후 구독 정보 수신
       ref.read(userSubscribeProvider.notifier).getSubscription();
-      _route(ref);
-    } catch (e) {
+
+      // 로그인 결과에 따른 라우팅 처리
+      _route(router);
+    } catch (e, s) {
       ToastService.showError('소셜 로그인에 실패했어요.', showImmediately: true);
+
+      FirebaseCrashlytics.instance.recordError(e, s, reason: '소셜 로그인 오류');
     }
   }
 }
