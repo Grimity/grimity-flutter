74dd15663e159169dee337cb7617467afe45278d
wonyong <wanyong0919@naver.com>
2025-11-10 00:23:51 +0900

    fix: 피드, 게시글 삭제 이후 context가 사라지는 경우 pop이 안되는 문제 수정

--- lib/presentation/post_detail/widget/post_detail_delete_dialog.dart
+++ lib/presentation/post_detail/widget/post_detail_delete_dialog.dart
@@ -1,25 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:grimity/app/config/app_router.dart';
 import 'package:grimity/presentation/common/widget/alert/grimity_dialog.dart';
 import 'package:grimity/presentation/post_detail/provider/post_detail_data_provider.dart';
 
 void showDeletePostDialog(String postId, BuildContext context, WidgetRef ref) {
+  final router = AppRouter.router(ref);
+
   showDialog(
     context: context,
     builder:
-        (context) => GrimityDialog(
+        (dialogContext) => GrimityDialog(
           title: '게시글을 삭제할까요?',
           content: '삭제 이후 되돌릴 수 없어요',
           cancelText: '취소',
           confirmText: '삭제',
-          onCancel: () => context.pop(),
+          onCancel: () => dialogContext.pop(),
           onConfirm: () async {
-            context.pop();
+            dialogContext.pop();
             final result = await ref.read(postDetailDataProvider(postId).notifier).deletePost(postId);
-            if (result && context.mounted) {
-              context.pop();
-            }
+
+            // 삭제가 성공한 경우 Pop
+            if (result) router.pop();
           },
         ),
   );
