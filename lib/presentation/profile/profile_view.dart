b9d3baafc918f8172db0e62c4dbf599d6925f17d
wonyong <wanyong0919@naver.com>
2025-11-10 15:51:45 +0900

    feat: 프로필 페이지 배경색 지정
    - 중첩된 페이지에서 페이지가 Push/Pop 될 때 뒤 페이지 화면이 겹쳐 보이는 문제가 있어 수정

--- lib/presentation/profile/profile_view.dart
+++ lib/presentation/profile/profile_view.dart
@@ -34,95 +34,99 @@ class ProfileView extends HookConsumerWidget {
     final userProfileKey = useMemoized(() => GlobalKey());
     final tabController = useTabController(initialLength: postTabView == null ? 1 : 2);
 
-    return SafeArea(
-      child: NotificationListener<ScrollNotification>(
-        onNotification: (scrollNotification) {
-          if (scrollNotification is ScrollUpdateNotification) {
-            final RenderBox? renderBox = userProfileKey.currentContext?.findRenderObject() as RenderBox?;
-            final double offset = scrollNotification.metrics.pixels;
+    return Container(
+      // 페이지 이동 시 뒤의 페이지가 보이는 문제가 있어 색상 지정 처리.
+      color: Colors.white,
+      child: SafeArea(
+        child: NotificationListener<ScrollNotification>(
+          onNotification: (scrollNotification) {
+            if (scrollNotification is ScrollUpdateNotification) {
+              final RenderBox? renderBox = userProfileKey.currentContext?.findRenderObject() as RenderBox?;
+              final double offset = scrollNotification.metrics.pixels;
 
-            if (renderBox != null) {
-              final profileHeight = renderBox.size.height;
+              if (renderBox != null) {
+                final profileHeight = renderBox.size.height;
 
-              final double fadeStartOffset = profileHeight * 0.6;
-              final double fadeEndOffset = profileHeight * 0.8;
+                final double fadeStartOffset = profileHeight * 0.6;
+                final double fadeEndOffset = profileHeight * 0.8;
 
-              if (offset <= fadeStartOffset) {
-                nameOpacity.value = 0.0;
-              } else if (offset >= fadeEndOffset) {
-                nameOpacity.value = 1.0;
+                if (offset <= fadeStartOffset) {
+                  nameOpacity.value = 0.0;
+                } else if (offset >= fadeEndOffset) {
+                  nameOpacity.value = 1.0;
+                } else {
+                  nameOpacity.value = (offset - fadeStartOffset) / (fadeEndOffset - fadeStartOffset);
+                }
               } else {
-                nameOpacity.value = (offset - fadeStartOffset) / (fadeEndOffset - fadeStartOffset);
-              }
-            } else {
-              const double fadeStartOffset = 150.0;
-              const double fadeEndOffset = 250.0;
+                const double fadeStartOffset = 150.0;
+                const double fadeEndOffset = 250.0;
 
-              if (offset <= fadeStartOffset) {
-                nameOpacity.value = 0.0;
-              } else if (offset >= fadeEndOffset) {
-                nameOpacity.value = 1.0;
-              } else {
-                nameOpacity.value = (offset - fadeStartOffset) / (fadeEndOffset - fadeStartOffset);
+                if (offset <= fadeStartOffset) {
+                  nameOpacity.value = 0.0;
+                } else if (offset >= fadeEndOffset) {
+                  nameOpacity.value = 1.0;
+                } else {
+                  nameOpacity.value = (offset - fadeStartOffset) / (fadeEndOffset - fadeStartOffset);
+                }
               }
             }
-          }
-          return false;
-        },
-        child: AppBarConnection(
-          appBars: [
-            AppBar(
-              behavior: AbsoluteAppBarBehavior(),
-              body: ProfileAppBar(userName: user.name, nameOpacity: nameOpacity.value, viewType: viewType),
-            ),
-            AppBar(behavior: MaterialAppBarBehavior(), body: Container(key: userProfileKey, child: userProfileView)),
-            AppBar(
-              behavior: MaterialAppBarBehavior(floating: true),
-              body: ProfileTabBar(user: user, tabController: tabController, viewType: viewType),
-            ),
-          ],
-          child: TabBarView(
-            controller: tabController,
-            physics: const NeverScrollableScrollPhysics(),
-            children: [
-              // pull to refresh 를 프로필 전체 기준으로 하려고 했으나
-              // 구조상 하나의 스크롤로 잡기가 어려워 탭 View 기준으로 refresh 처리
-              // refresh시 해당 탭 + 사용자 정보까지 업데이트 처리
-              GrimityRefreshIndicator(
-                onRefresh: () async {
-                  await Future.wait([
-                    ref.refresh(profileFeedsDataProvider(user.id).future),
-                    ref.refresh(profileDataProvider(user.url).future),
-                  ]);
-                },
-                child: GrimityInfiniteScrollPagination(
-                  isEnabled:
-                      user.id.isNotEmpty &&
-                      ref.watch(profileFeedsDataProvider(user.id)).valueOrNull?.nextCursor != null,
-                  onLoadMore: () => ref.read(profileFeedsDataProvider(user.id).notifier).loadMore(user.id),
-                  child: feedTabView,
-                ),
+            return false;
+          },
+          child: AppBarConnection(
+            appBars: [
+              AppBar(
+                behavior: AbsoluteAppBarBehavior(),
+                body: ProfileAppBar(userName: user.name, nameOpacity: nameOpacity.value, viewType: viewType),
               ),
-
-              if (postTabView != null)
+              AppBar(behavior: MaterialAppBarBehavior(), body: Container(key: userProfileKey, child: userProfileView)),
+              AppBar(
+                behavior: MaterialAppBarBehavior(floating: true),
+                body: ProfileTabBar(user: user, tabController: tabController, viewType: viewType),
+              ),
+            ],
+            child: TabBarView(
+              controller: tabController,
+              physics: const NeverScrollableScrollPhysics(),
+              children: [
+                // pull to refresh 를 프로필 전체 기준으로 하려고 했으나
+                // 구조상 하나의 스크롤로 잡기가 어려워 탭 View 기준으로 refresh 처리
+                // refresh시 해당 탭 + 사용자 정보까지 업데이트 처리
                 GrimityRefreshIndicator(
                   onRefresh: () async {
                     await Future.wait([
-                      ref.refresh(profilePostsDataProvider(user.id).future),
+                      ref.refresh(profileFeedsDataProvider(user.id).future),
                       ref.refresh(profileDataProvider(user.url).future),
                     ]);
                   },
                   child: GrimityInfiniteScrollPagination(
                     isEnabled:
-                        (() {
-                          final posts = ref.watch(profilePostsDataProvider(user.id)).valueOrNull;
-                          return user.id.isNotEmpty && posts != null && posts.length % 10 == 0;
-                        })(),
-                    onLoadMore: () => ref.read(profilePostsDataProvider(user.id).notifier).loadMore(user.id),
-                    child: postTabView!,
+                        user.id.isNotEmpty &&
+                        ref.watch(profileFeedsDataProvider(user.id)).valueOrNull?.nextCursor != null,
+                    onLoadMore: () => ref.read(profileFeedsDataProvider(user.id).notifier).loadMore(user.id),
+                    child: feedTabView,
                   ),
                 ),
-            ],
+
+                if (postTabView != null)
+                  GrimityRefreshIndicator(
+                    onRefresh: () async {
+                      await Future.wait([
+                        ref.refresh(profilePostsDataProvider(user.id).future),
+                        ref.refresh(profileDataProvider(user.url).future),
+                      ]);
+                    },
+                    child: GrimityInfiniteScrollPagination(
+                      isEnabled:
+                          (() {
+                            final posts = ref.watch(profilePostsDataProvider(user.id)).valueOrNull;
+                            return user.id.isNotEmpty && posts != null && posts.length % 10 == 0;
+                          })(),
+                      onLoadMore: () => ref.read(profilePostsDataProvider(user.id).notifier).loadMore(user.id),
+                      child: postTabView!,
+                    ),
+                  ),
+              ],
+            ),
           ),
         ),
       ),
