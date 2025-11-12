8fbfb663e7d4774395a75130694a5fc0b90effb9
Dev Ttangkong <ttankkeo112@gmail.com>
2025-11-12 15:25:51 +0900

    댓글 아이콘 클릭 시 키보드를 포커스하도록 업데이트

--- lib/presentation/common/widget/grimity_util_bar.dart
+++ lib/presentation/common/widget/grimity_util_bar.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gap/gap.dart';
 import 'package:grimity/app/config/app_color.dart';
 import 'package:grimity/app/config/app_typeface.dart';
 import 'package:grimity/gen/assets.gen.dart';
+import 'package:grimity/presentation/comment/enum/comment_type.dart';
+import 'package:grimity/presentation/comment/provider/comment_input_provider.dart';
 import 'package:grimity/presentation/common/widget/grimity_animation_button.dart';
+import 'package:grimity/presentation/common/widget/grimity_gesture.dart';
 import 'package:grimity/presentation/common/widget/popup/grimity_share_modal_bottom_sheet.dart';
 
 /// 공통 UtilBar
 /// 좋아요, 저장, 댓글, 공유 기능 UtilBar
-class GrimityUtilBar extends StatelessWidget {
+class GrimityUtilBar extends ConsumerWidget {
   const GrimityUtilBar({
     super.key,
     required this.isLike,
@@ -21,6 +25,7 @@ class GrimityUtilBar extends StatelessWidget {
     required this.shareContentType,
     required this.title,
     required this.thumbnail,
+    required this.commentType,
   });
 
   final bool isLike;
@@ -31,6 +36,7 @@ class GrimityUtilBar extends StatelessWidget {
   final ShareContentType shareContentType;
   final String title;
   final String? thumbnail;
+  final CommentType commentType;
 
   final VoidCallback onLikeTap;
   final VoidCallback onSaveTap;
@@ -56,6 +62,7 @@ class GrimityUtilBar extends StatelessWidget {
     shareContentType: ShareContentType.feed,
     title: title,
     thumbnail: thumbnail,
+    commentType: CommentType.feed,
   );
 
   factory GrimityUtilBar.post({
@@ -79,10 +86,11 @@ class GrimityUtilBar extends StatelessWidget {
     shareContentType: ShareContentType.post,
     title: title,
     thumbnail: thumbnail,
+    commentType: CommentType.post,
   );
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Container(
       color: AppColor.gray00,
       height: 50,
@@ -101,9 +109,21 @@ class GrimityUtilBar extends StatelessWidget {
               Gap(6),
               Text('$likeCount', style: AppTypeface.label3.copyWith(color: AppColor.gray700)),
               Gap(20),
-              Assets.icons.common.reply.svg(width: 24, height: 24),
-              Gap(6),
-              Text('$commentCount', style: AppTypeface.label3.copyWith(color: AppColor.gray700)),
+              GrimityGesture(
+                onTap: () {
+                  final notifier = ref.watch(commentInputProvider(commentType).notifier);
+                  assert(notifier.focusNode != null);
+                  notifier.focusNode?.requestFocus();
+                },
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  spacing: 6,
+                  children: [
+                    Assets.icons.common.reply.svg(width: 24, height: 24),
+                    Text('$commentCount', style: AppTypeface.label3.copyWith(color: AppColor.gray700)),
+                  ],
+                ),
+              ),
             ],
           ),
           Row(
