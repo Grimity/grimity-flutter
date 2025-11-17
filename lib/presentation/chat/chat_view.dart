acd4235cacf988b94515b6c4fe538d8c93e91ead
Dev Ttangkong <ttankkeo112@gmail.com>
2025-11-17 23:03:33 +0900

    #106 누락된 SafeArea 위젯 추가

--- lib/presentation/chat/chat_view.dart
+++ lib/presentation/chat/chat_view.dart
@@ -53,63 +53,65 @@ class _ChatViewState extends ConsumerState<ChatView> {
     return Scaffold(
       endDrawer: widget.drawerView,
       appBar: widget.appbarView,
-      body: Consumer(
-        builder: (context, ref, _) {
-          final provider = ref.read(chatProviderProvider.notifier);
-          final data = ref.watch(chatProviderProvider);
+      body: SafeArea(
+        child: Consumer(
+          builder: (context, ref, _) {
+            final provider = ref.read(chatProviderProvider.notifier);
+            final data = ref.watch(chatProviderProvider);
 
-          // 현재 데이터를 불러오고 있는 경우.
-          if (data.isLoading) {
-            return Center(child: GrimityCircularProgressIndicator());
-          }
+            // 현재 데이터를 불러오고 있는 경우.
+            if (data.isLoading) {
+              return Center(child: GrimityCircularProgressIndicator());
+            }
 
-          // 현재 주고 받은 메세지가 아직 없는 경우.
-          if (data.value!.chats.isEmpty && data.value!.keyword == null) {
-            return ListView(
+            // 현재 주고 받은 메세지가 아직 없는 경우.
+            if (data.value!.chats.isEmpty && data.value!.keyword == null) {
+              return ListView(
+                children: [
+                  GrimityStateView.commentReply(
+                    title: "주고 받은 메세지가 없어요",
+                    subTitle: "팔로우 한 작가에게 메세지를 보내보세요",
+                    buttonText: "새 메세지 보내기",
+                    onTap: () => NewChatRoute().push(context),
+                  ),
+                ],
+              );
+            }
+
+            return Stack(
               children: [
-                GrimityStateView.commentReply(
-                  title: "주고 받은 메세지가 없어요",
-                  subTitle: "팔로우 한 작가에게 메세지를 보내보세요",
-                  buttonText: "새 메세지 보내기",
-                  onTap: () => NewChatRoute().push(context),
+                Column(
+                  spacing: 16,
+                  children: [
+                    widget.searchBarView,
+                    widget.toolBarView,
+                    Expanded(
+                      child: GrimityRefreshIndicator(
+                        onRefresh: provider.refresh,
+                        child: ListView.separated(
+                          separatorBuilder: (context, index) {
+                            return SizedBox(height: 16);
+                          },
+                          padding: EdgeInsets.all(16),
+                          itemCount: data.value!.chats.length,
+                          itemBuilder: (context, index) {
+                            return ChatScrollItem(model: data.value!.chats[index]);
+                          },
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+
+                // 오른쪽 하단 액션 버튼 표시.
+                Align(
+                  alignment: Alignment.bottomRight,
+                  child: Padding(padding: EdgeInsets.only(right: 16, bottom: 16), child: ChatFloatingActionButton()),
                 ),
               ],
             );
-          }
-
-          return Stack(
-            children: [
-              Column(
-                spacing: 16,
-                children: [
-                  widget.searchBarView,
-                  widget.toolBarView,
-                  Expanded(
-                    child: GrimityRefreshIndicator(
-                      onRefresh: provider.refresh,
-                      child: ListView.separated(
-                        separatorBuilder: (context, index) {
-                          return SizedBox(height: 16);
-                        },
-                        padding: EdgeInsets.all(16),
-                        itemCount: data.value!.chats.length,
-                        itemBuilder: (context, index) {
-                          return ChatScrollItem(model: data.value!.chats[index]);
-                        },
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-
-              // 오른쪽 하단 액션 버튼 표시.
-              Align(
-                alignment: Alignment.bottomRight,
-                child: Padding(padding: EdgeInsets.only(right: 16, bottom: 16), child: ChatFloatingActionButton()),
-              ),
-            ],
-          );
-        },
+          },
+        ),
       ),
     );
   }
--- lib/presentation/chat_new/new_chat_view.dart
+++ lib/presentation/chat_new/new_chat_view.dart
@@ -17,46 +17,50 @@ class NewChatView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appBarView,
-      body: Consumer(
-        builder: (context, ref, child) {
-          final data = ref.watch(newChatProviderProvider);
+      body: SafeArea(
+        child: Consumer(
+          builder: (context, ref, child) {
+            final data = ref.watch(newChatProviderProvider);
 
-          // 현재 데이터를 불러오고 있는 경우.
-          if (data.isLoading) {
-            return Center(child: GrimityCircularProgressIndicator());
-          }
+            // 현재 데이터를 불러오고 있는 경우.
+            if (data.isLoading) {
+              return Center(child: GrimityCircularProgressIndicator());
+            }
 
-          // 현재 팔로잉하고 있는 사용자가 아예 없는 경우.
-          if (data.value!.followings.isEmpty && data.value!.keyword == null) {
-            return ListView(
-              children: [GrimityStateView.user(title: "아직 팔로우하는 작가가 없어요", subTitle: "관심 있는 작가를 팔로우하고\n메세지를 주고 받아보세요")],
-            );
-          }
+            // 현재 팔로잉하고 있는 사용자가 아예 없는 경우.
+            if (data.value!.followings.isEmpty && data.value!.keyword == null) {
+              return ListView(
+                children: [
+                  GrimityStateView.user(title: "아직 팔로우하는 작가가 없어요", subTitle: "관심 있는 작가를 팔로우하고\n메세지를 주고 받아보세요"),
+                ],
+              );
+            }
 
-          return Column(
-            children: [
-              searchBarView,
-              Expanded(
-                child: GrimityInfiniteScrollPagination(
-                  isEnabled: data.value!.nextCursor != null,
-                  onLoadMore: ref.read(newChatProviderProvider.notifier).loadMore,
-                  child: ListView.separated(
-                    padding: EdgeInsets.all(16),
-                    shrinkWrap: true,
-                    separatorBuilder: (context, index) {
-                      return SizedBox(height: 16);
-                    },
-                    itemCount: data.value!.followings.length,
-                    itemBuilder: (context, index) {
-                      return NewChatScrollItemView(model: data.value!.followings[index]);
-                    },
+            return Column(
+              children: [
+                searchBarView,
+                Expanded(
+                  child: GrimityInfiniteScrollPagination(
+                    isEnabled: data.value!.nextCursor != null,
+                    onLoadMore: ref.read(newChatProviderProvider.notifier).loadMore,
+                    child: ListView.separated(
+                      padding: EdgeInsets.all(16),
+                      shrinkWrap: true,
+                      separatorBuilder: (context, index) {
+                        return SizedBox(height: 16);
+                      },
+                      itemCount: data.value!.followings.length,
+                      itemBuilder: (context, index) {
+                        return NewChatScrollItemView(model: data.value!.followings[index]);
+                      },
+                    ),
                   ),
                 ),
-              ),
-              sibmitButtonView,
-            ],
-          );
-        },
+                sibmitButtonView,
+              ],
+            );
+          },
+        ),
       ),
     );
   }
