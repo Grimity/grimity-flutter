3985d54f05562338ca82e82a0a0907e023d27e6f
Dev Ttangkong <ttankkeo112@gmail.com>
2025-11-24 22:51:20 +0900

    더 나은 초기 세팅을 위한 tools/setup.dart 파일 추가

---
+++ tools/setup.dart
@@ -0,0 +1,26 @@
+import 'dart:io';
+
+/// 주어진 실행 파일과 인자를 이용하여 프로세스를 실행하고,
+/// stdout과 stderr를 실시간으로 콘솔에 출력합니다.
+Future<void> run(String executable, List<String> arguments) async {
+  final process = await Process.start(
+    executable,
+    arguments,
+    runInShell: true,
+  );
+
+  // stdout 실시간 출력
+  process.stdout.transform(SystemEncoding().decoder).listen((data) {
+    stdout.write(data);
+  });
+
+  // stderr 실시간 출력
+  process.stderr.transform(SystemEncoding().decoder).listen((data) {
+    stderr.write(data);
+  });
+}
+
+void main() async {
+  await run("dart", ["run", "git_config", "fetch"]);
+  await run("dart", ["run", "build_runner", "build", "--delete-conflicting-outputs"]);
+}
