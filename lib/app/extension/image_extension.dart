e545b4b90def148ebb5b122009495122e9eb91c5
Dev Ttangkong <ttankkeo112@gmail.com>
2025-11-30 18:03:00 +0900

    GrimityCachedNetworkImage에서 주어진 크기가 무한일 때 발생하는 치명적 예외 해결

--- lib/app/extension/image_extension.dart
+++ lib/app/extension/image_extension.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 extension ImageExtension on num {
   // 캐시 사이즈
   int cacheSize(BuildContext context) {
+    assert(!isInfinite, "이미지 캐시 크기는 유한해야 합니다.");
+    assert(!isNegative, "이미지 캐시 크기는 음수가 될 수 없습니다.");
+
     // 위젯 사이즈 x 디바이스 픽셀 비율
     return (this * MediaQuery.of(context).devicePixelRatio).round();
   }
