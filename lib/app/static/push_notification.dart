e6502220bf08b37878069f82165e52cc9d098d32
Dev Ttangkong <ttankkeo112@gmail.com>
2025-12-03 14:07:38 +0900

    캐스팅하도록 다시 롤백

--- lib/app/static/push_notification.dart
+++ lib/app/static/push_notification.dart
@@ -133,9 +133,6 @@ class PushNotification {
 
   /// 앱이 포그라운드인 상태에서 푸시 알림 메시지가 전송되었을 때 호출됩니다.
   static void onForegroundMessage(RemoteMessage message) async {
-    // 포그라운드 상태에서는 별도의 알림 배지를 표시할 필요가 없음.
-    FlutterAppBadge.count(0);
-
     if (_streamController.hasListener) {
       _streamController.add(message);
       return;
