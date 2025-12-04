db955fe4bd647391f77152539674be042ef9f83a
Dev Ttangkong <ttankkeo112@gmail.com>
2025-12-04 22:33:27 +0900

    추천 작가 피드 프로필 페이지 진입에 대한 상호작용 추가

--- lib/presentation/common/widget/user_card/grimity_author_with_feeds_card.dart
+++ lib/presentation/common/widget/user_card/grimity_author_with_feeds_card.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:grimity/app/config/app_color.dart';
+import 'package:grimity/app/config/app_router.dart';
 import 'package:grimity/app/config/app_typeface.dart';
 import 'package:grimity/app/extension/build_context_extension.dart';
 import 'package:grimity/app/extension/image_extension.dart';
 import 'package:grimity/gen/assets.gen.dart';
 import 'package:grimity/presentation/common/provider/author_with_feeds_provider.dart';
 import 'package:grimity/presentation/common/widget/button/grimity_button.dart';
+import 'package:grimity/presentation/common/widget/grimity_gesture.dart';
 import 'package:grimity/presentation/common/widget/grimity_image.dart';
 import 'package:grimity/presentation/common/widget/grimity_reaction.dart';
 import 'package:grimity/presentation/common/widget/system/profile/grimity_user_image.dart';
@@ -18,58 +20,67 @@ class GrimityAuthorWithFeedsCard extends StatelessWidget {
   final AuthorWithFeeds authorWithFeeds;
   final VoidCallback onFollowTab;
 
+  /// 인기 유저의 프로필 페이지로 이동.
+  void goProfile(BuildContext context) {
+    final user = authorWithFeeds.user;
+    ProfileRoute(url: user.url).push(context);
+  }
+
   @override
   Widget build(BuildContext context) {
     final user = authorWithFeeds.user;
     final feeds = authorWithFeeds.feeds;
 
-    return Container(
-      padding: EdgeInsets.all(16),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(12),
-        border: Border.all(color: AppColor.gray300, width: 1),
-      ),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Row(
-            children: [
-              GrimityUserImage(imageUrl: user.image, size: 30),
-              Gap(8),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(user.name, style: AppTypeface.label2.copyWith(color: AppColor.gray700)),
-                  GrimityReaction.follower(followerCount: user.followerCount),
-                ],
-              ),
-              Spacer(),
-              GrimityButton.follow(isFollowing: user.isFollowing ?? false, onTap: onFollowTab),
-            ],
-          ),
-          Gap(20),
-          Row(
-            spacing: 4,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: List.generate(context.authorFeedRowCount, (index) {
-              if (index < feeds.length) {
-                return Expanded(
-                  child: AspectRatio(
-                    aspectRatio: 1.0,
-                    child: GrimityImage.infinity(imageUrl: feeds[index].thumbnail ?? ''),
-                  ),
-                );
-              } else {
-                return Expanded(
-                  child: AspectRatio(
-                    aspectRatio: 1.0,
-                    child: Assets.images.imagePlaceholder.image(height: 110, cacheHeight: 110.cacheSize(context)),
-                  ),
-                );
-              }
-            }),
-          ),
-        ],
+    return GrimityGesture(
+      onTap: () => goProfile(context),
+      child: Container(
+        padding: EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12),
+          border: Border.all(color: AppColor.gray300, width: 1),
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Row(
+              children: [
+                GrimityUserImage(imageUrl: user.image, size: 30),
+                Gap(8),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(user.name, style: AppTypeface.label2.copyWith(color: AppColor.gray700)),
+                    GrimityReaction.follower(followerCount: user.followerCount),
+                  ],
+                ),
+                Spacer(),
+                GrimityButton.follow(isFollowing: user.isFollowing ?? false, onTap: onFollowTab),
+              ],
+            ),
+            Gap(20),
+            Row(
+              spacing: 4,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: List.generate(context.authorFeedRowCount, (index) {
+                if (index < feeds.length) {
+                  return Expanded(
+                    child: AspectRatio(
+                      aspectRatio: 1.0,
+                      child: GrimityImage.infinity(imageUrl: feeds[index].thumbnail ?? ''),
+                    ),
+                  );
+                } else {
+                  return Expanded(
+                    child: AspectRatio(
+                      aspectRatio: 1.0,
+                      child: Assets.images.imagePlaceholder.image(height: 110, cacheHeight: 110.cacheSize(context)),
+                    ),
+                  );
+                }
+              }),
+            ),
+          ],
+        ),
       ),
     );
   }
