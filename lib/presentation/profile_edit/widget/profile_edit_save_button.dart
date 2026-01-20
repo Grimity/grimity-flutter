c78c7cf0c2951a0560a6a9d1a3e444e9311697b7
wonyong <wanyong0919@naver.com>
2026-01-20 15:09:32 +0900

    fix: 프로필 편집 후 화면 갱신 오류 수정

--- lib/presentation/profile_edit/widget/profile_edit_save_button.dart
+++ lib/presentation/profile_edit/widget/profile_edit_save_button.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:grimity/app/config/app_router.dart';
 import 'package:grimity/presentation/common/widget/button/grimity_button.dart';
 import 'package:grimity/presentation/profile/provider/profile_data_provider.dart';
 import 'package:grimity/presentation/profile_edit/provider/profile_edit_provider.dart';
@@ -20,10 +21,24 @@ class ProfileEditSaveButton extends ConsumerWidget {
         child: GrimityButton.large(
           text: '변경 내용 저장',
           onTap: () async {
+            final router = ref.read(routerProvider);
+
             await ref.read(profileEditProvider.notifier).updateUser();
             if (context.mounted && ref.read(profileEditProvider).isSaved == true) {
+              final newUrl = ref.read(profileEditProvider).url;
+              // ProfileEdit 페이지 Pop
+              if (router.canPop()) {
+                router.pop();
+              }
+
+              // 기존 Profile 페이지에서 사용하는 데이터 무효화
               ref.invalidate(profileDataProvider);
-              Navigator.of(context).pop();
+
+              // 변경된 URL 기준으로 프로필 페이지 pushReplacement
+              Future.delayed(
+                const Duration(milliseconds: 100),
+                () => router.pushReplacement(ProfileRoute.makePath(newUrl)),
+              );
             }
           },
         ),
