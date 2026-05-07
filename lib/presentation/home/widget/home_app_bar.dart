f1e4b24a2e623999044b415bbd16231657258c0b
LeeJueon <loolloo080313@gmail.com>
2026-05-07 19:37:26 +0900

    fix: remove unused home app bar import

--- lib/presentation/home/widget/home_app_bar.dart
+++ lib/presentation/home/widget/home_app_bar.dart
@@ -3,7 +3,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gds/gds.dart';
 import 'package:grimity/app/config/app_router.dart';
 import 'package:grimity/gen/assets.gen.dart';
-import 'package:grimity/domain/entity/user.dart';
 import 'package:grimity/presentation/common/provider/user_auth_provider.dart';
 
 class HomeAppBar extends ConsumerWidget {
