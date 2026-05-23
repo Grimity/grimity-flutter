ec092bcabad6c7c3510bc18d5e7983354d0df229
Dev Ttangkong <ttankkeo112@gmail.com>
2026-05-23 16:16:30 +0900

    feat: 앱 업데이트 다이얼로그에 디자인 시스템 적용

--- lib/presentation/app_update/show_app_update_dialog.dart
+++ lib/presentation/app_update/show_app_update_dialog.dart
@@ -1,17 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:gds/gds.dart';
 import 'package:grimity/app/update/update_factory.dart';
-import 'package:grimity/presentation/common/widget/alert/grimity_dialog.dart';
 
-void showAppUpdateDialog(BuildContext context) {
-  showDialog(
-    context: context,
-    barrierDismissible: false,
-    builder:
-        (context) => GrimityDialog(
-          title: 'Grimity 업데이트 알림',
-          content: '더 나은 서비스를 위해 그리미티 앱이\n업데이트 되었습니다. 최신 앱을 설치해주세요.',
-          confirmText: '앱 업데이트',
-          onConfirm: () => UpdateServiceFactory.create().forceUpdate(),
-        ),
+Future<void> showAppUpdateDialog(BuildContext context) {
+  final alert = GdsAlert(
+    type: GdsAlertType.normal,
+    size: context.isMobile ? GdsAlertSize.md : GdsAlertSize.xl,
+    title: 'Grimity 업데이트 알림',
+    description: '더 나은 서비스를 위해 그리미티 앱이\n업데이트 되었습니다. 최신 앱을 설치해주세요.',
+    primaryLabel: '앱 업데이트',
+    onPrimaryTap: () => UpdateServiceFactory.create().forceUpdate(),
   );
+
+  return alert.open(context);
 }
