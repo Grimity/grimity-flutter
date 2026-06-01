d563bc0bda9886924b0307cd0ef5678020981ba9
Dev Ttangkong <ttankkeo112@gmail.com>
2026-06-01 22:42:43 +0900

    feat: 차단 해제 페이지에 디자인 시스템 적용

--- lib/presentation/block/view/blocked_users_view.dart
+++ lib/presentation/block/view/blocked_users_view.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:gap/gap.dart';
+import 'package:gds/gds.dart';
 import 'package:grimity/domain/entity/user.dart';
 import 'package:grimity/presentation/block/provider/blocked_users_data_provider.dart';
-import 'package:grimity/presentation/common/widget/button/grimity_button.dart';
 import 'package:grimity/presentation/common/widget/grimity_state_view.dart';
-import 'package:grimity/presentation/common/widget/system/profile/grimity_user_profile.dart';
 import 'package:skeletonizer/skeletonizer.dart';
 
 class BlockedUsersView extends ConsumerWidget {
@@ -15,23 +13,27 @@ class BlockedUsersView extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final blockedUsers = ref.watch(blockedUsersDataProvider);
 
-    return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-      child: blockedUsers.when(
-        data: (data) {
-          final users = data;
+    return blockedUsers.when(
+      data: (data) {
+        final users = data;
 
-          if (users.isEmpty) {
-            return GrimityStateView.resultNull(
-              subTitle: '아직 차단한 유저가 없어요',
-            );
-          }
+        if (users.isEmpty) {
+          return Container(
+            padding: EdgeInsets.only(
+              top: GdsSpacing.spacing16,
+              left: GdsSpacing.spacing16,
+              right: GdsSpacing.spacing16,
+              bottom: GdsSpacing.spacing40,
+            ),
+            alignment: Alignment.center,
+            child: GdsEmptyState(title: '차단한 작가가 없어요', icon: GdsIcon.warning),
+          );
+        }
 
-          return BlockedUserListView(users: users);
-        },
-        loading: () => Skeletonizer(child: BlockedUserListView(users: User.emptyList)),
-        error: (e, s) => GrimityStateView.error(onTap: () => ref.invalidate(blockedUsersDataProvider)),
-      ),
+        return BlockedUserListView(users: users);
+      },
+      loading: () => Skeletonizer(child: BlockedUserListView(users: User.emptyList)),
+      error: (e, s) => GrimityStateView.error(onTap: () => ref.invalidate(blockedUsersDataProvider)),
     );
   }
 }
@@ -43,29 +45,22 @@ class BlockedUserListView extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return ListView.separated(
+    return ListView.builder(
+      padding: EdgeInsets.all(GdsSpacing.spacing16),
       itemCount: users.length,
       itemBuilder: (context, index) {
         final user = users[index];
 
-        return Row(
-          children: [
-            Expanded(
-              child: GrimityUserProfile.fromString(
-                title: user.name,
-                imageUrl: user.image,
-              ),
-            ),
-            GrimityButton.round(
-              text: '차단 해제',
-              onTap: () => ref.read(blockedUsersDataProvider.notifier).unblockUser(user.id),
-              style: ButtonStyleType.line,
-            ),
-          ],
+        return GdsUserItem.id(
+          userId: user.handle,
+          nickName: user.name,
+          personAvatar: user.personAvatar,
+          secondaryActionButton: GdsOutlinedButton(
+            size: GdsOutlinedButtonSize.small,
+            text: '차단 해제',
+            onPressed: () => ref.read(blockedUsersDataProvider.notifier).unblockUser(user.id),
+          ),
         );
-      },
-      separatorBuilder: (context, index) {
-        return Gap(16);
       },
     );
   }
