155481de765e13efed2de4b700a5d3fc011b14f3
Dev Ttangkong <ttankkeo112@gmail.com>
2026-06-05 14:44:57 +0900

    feat: 사업자 정보 페이지에 디자인 시스템 적용

--- lib/presentation/business_info/business_info_page.dart
+++ lib/presentation/business_info/business_info_page.dart
@@ -1,63 +1,65 @@
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
-import 'package:grimity/app/config/app_color.dart';
-import 'package:grimity/app/config/app_theme.dart';
-import 'package:grimity/app/config/app_typeface.dart';
+import 'package:gds/gds.dart';
+import 'package:go_router/go_router.dart';
 
 class BusinessInfoPage extends StatelessWidget {
   const BusinessInfoPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        toolbarHeight: AppTheme.kToolbarHeight.height,
-        titleSpacing: 0,
-        bottom: const PreferredSize(
-          preferredSize: Size.fromHeight(1),
-          child: Divider(height: 1, color: AppColor.gray300),
-        ),
+    final colors = context.gdsColors;
+
+    return GdsScaffold(
+      appBar: GdsTopNavigation.iconButton(
+        title: '사업자 정보',
+        icons: [],
+        onBack: context.pop,
+        onIconTap: [],
+        showTitle: false,
+        showIcons: false,
       ),
       body: SafeArea(
         child: ListView(
-          padding: const EdgeInsets.all(16.0),
+          padding: EdgeInsets.all(GdsSpacing.spacing16),
           children: [
             Text(
               '사업자 정보',
-              style: AppTypeface.title2,
+              style: GdsTypography.title1.copyWith(color: colors.text.grayBold),
             ),
             Gap(24),
-            _buildInfoItem('사업자 등록 번호', '408-27-02500'),
-            Gap(16),
-            _buildInfoItem('법인 여부', '개인'),
-            Gap(16),
-            _buildInfoItem('상호', '그리미티 (Grimity)'),
-            Gap(16),
-            _buildInfoItem('대표자명', '임종훈'),
-            Gap(16),
-            _buildInfoItem('전화번호', '070-8098-7916'),
-            Gap(16),
-            _buildInfoItem('개업일', '2025-10-11'),
-            Gap(16),
-            _buildInfoItem('전자우편', 'grimity.official@gmail.com'),
-            Gap(16),
-            _buildInfoItem('사업장소재지(도로명)', '부산광역시 사상구 가야대로255번길 5, 107동 104호'),
-            Gap(16),
-            _buildInfoItem('인터넷 도메인', 'https://www.grimity.com/'),
-            Gap(16),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              spacing: GdsSpacing.spacing16,
+              children: [
+                _buildInfoItem(context, '사업자 등록 번호', '408-27-02500'),
+                _buildInfoItem(context, '법인 여부', '개인'),
+                _buildInfoItem(context, '상호', '그리미티 (Grimity)'),
+                _buildInfoItem(context, '대표자명', '임종훈'),
+                _buildInfoItem(context, '전화번호', '070-8098-7916'),
+                _buildInfoItem(context, '개업일', '2025-10-11'),
+                _buildInfoItem(context, '전자우편', 'grimity.official@gmail.com'),
+                _buildInfoItem(context, '사업장소재지(도로명)', '부산광역시 사상구 가야대로255번길 5, 107동 104호'),
+                _buildInfoItem(context, '인터넷 도메인', 'https://www.grimity.com/'),
+              ],
+            ),
           ],
         ),
       ),
     );
   }
 
-  Widget _buildInfoItem(String title, String content) {
+  Widget _buildInfoItem(BuildContext context, String title, String content) {
+    final colors = context.gdsColors;
+
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisSize: MainAxisSize.min,
+      spacing: GdsSpacing.spacing8,
       children: [
-        Text(title, style: AppTypeface.subTitle3),
-        Gap(8),
-        Text(content, style: AppTypeface.label3),
+        Text(title, style: GdsTypography.subtitle2.copyWith(color: colors.text.grayBold)),
+        Text(content, style: GdsTypography.body2R.copyWith(color: colors.text.grayBold)),
       ],
     );
   }
