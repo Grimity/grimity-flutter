cf087b29968ac40bd70b2bfb3d80a35470d7dd84
Dev Ttangkong <ttankkeo112@gmail.com>
2026-06-05 11:08:01 +0900

    feat: 자유게시판에서 GdsMenu 컴포넌트 적용

--- lib/app/enum/search_type.enum.dart
+++ lib/app/enum/search_type.enum.dart
@@ -2,11 +2,10 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 
 enum SearchType {
   @JsonValue('combined')
-  combined('제목+내용'),
+  combined('제목'),
   @JsonValue('name')
   name('글쓴이');
 
-  final String typeName;
-
-  const SearchType(this.typeName);
+  final String displayName;
+  const SearchType(this.displayName);
 }
