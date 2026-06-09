afcd8f8b9b12a6039aaea5b9751f91986f86be6e
Dev Ttangkong <ttankkeo112@gmail.com>
2026-06-10 00:15:11 +0900

    feat: 사이드바에 디자인 시스템 적용

--- lib/presentation/drawer/main_app_drawer.dart
+++ lib/presentation/drawer/main_app_drawer.dart
@@ -1,22 +1,105 @@
 import 'package:flutter/material.dart';
-import 'package:grimity/presentation/drawer/view/drawer_view.dart';
-import 'package:grimity/presentation/drawer/widget/drawer_close_button.dart';
-import 'package:grimity/presentation/drawer/widget/drawer_footer.dart';
-import 'package:grimity/presentation/drawer/widget/drawer_menu.dart';
-import 'package:grimity/presentation/drawer/widget/drawer_profile.dart';
-import 'package:grimity/presentation/drawer/widget/drawer_upload_button.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:gds/gds.dart';
+import 'package:go_router/go_router.dart';
+import 'package:grimity/app/config/app_const.dart';
+import 'package:grimity/app/config/app_router.dart';
+import 'package:grimity/app/enum/login_provider.enum.dart';
+import 'package:grimity/presentation/common/provider/user_auth_provider.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-class MainAppDrawer extends StatelessWidget {
+class MainAppDrawer extends ConsumerWidget {
   const MainAppDrawer({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return DrawerView(
-      closeButton: DrawerCloseButton(),
-      profileWidget: DrawerProfile(),
-      uploadButton: DrawerUploadButton(),
-      menuListView: DrawerMenuListView(),
-      drawerFooter: DrawerFooter(),
+  Widget build(BuildContext context, WidgetRef ref) {
+    final statusBarHeight = MediaQuery.of(context).viewPadding.top;
+    final user = ref.watch(userAuthProvider);
+
+    return GdsSidebarNavigation(
+      size: context.isMobile ? GdsSidebarNavigationSize.md : GdsSidebarNavigationSize.lg,
+      padding: EdgeInsets.only(top: statusBarHeight),
+      userImageUrl: user?.image,
+      userName: user?.name ?? '',
+      userId: user?.handle ?? '',
+      followerCount: user?.followerCount ?? 0,
+      followingCount: user?.followingCount ?? 0,
+      onAvatarTap: () => _pushProfile(context, user?.url),
+      onHandleTap: () => _pushProfile(context, user?.url),
+      onNickNameTap: () => _pushProfile(context, user?.url),
+      onFollowerTap: () => _push(context, FollowRoute.path),
+      onFollowingTap: () => _push(context, FollowRoute.path),
+      onTermsOfServiceTap: () => launchUrl(Uri.parse(AppConst.serviceTermsUrl)),
+      onPrivacyPolicyTap: () => launchUrl(Uri.parse(AppConst.privacyPolicyUrl)),
+      onBusinessInfoTap: () => _push(context, BusinessInfoRoute.path),
+      onSignOutTap: () => _signOut(context, ref),
+      menuItems: [
+        GdsSidebarNavigationItem(
+          icon: GdsIcon.home,
+          label: '홈',
+          onTap: () => _go(context, HomeRoute.path),
+        ),
+        GdsSidebarNavigationItem(
+          icon: GdsIcon.paint,
+          label: '그린 그림',
+          onTap: () => _go(context, RankingRoute.path),
+        ),
+        GdsSidebarNavigationItem(
+          icon: GdsIcon.board,
+          label: '자유 게시판',
+          onTap: () => _go(context, BoardRoute.path),
+        ),
+        GdsSidebarNavigationItem(
+          icon: GdsIcon.following,
+          label: '팔로잉',
+          onTap: () => _go(context, FollowingRoute.path),
+        ),
+        GdsSidebarNavigationItem(
+          icon: GdsIcon.message,
+          label: 'DM',
+          onTap: () => _go(context, ChatRoute.path),
+          dotPushBadge: user?.hasUnreadChatMessage ?? false,
+        ),
+        GdsSidebarNavigationItem(
+          icon: GdsIcon.inbox,
+          label: '보관함',
+          onTap: () => _push(context, StorageRoute.path),
+        ),
+        GdsSidebarNavigationItem(
+          icon: GdsIcon.settings,
+          label: '설정',
+          onTap: () => _push(context, SettingRoute.path),
+        ),
+      ],
     );
+  }
+
+  void _go(BuildContext context, String path) {
+    context.pop();
+    context.go(path);
+  }
+
+  void _push(BuildContext context, String path) {
+    context.pop();
+    context.push(path);
+  }
+
+  void _pushProfile(BuildContext context, String? url) {
+    if (url == null) return;
+
+    context.pop();
+    ProfileRoute(url: url).push(context);
+  }
+
+  Future<void> _signOut(BuildContext context, WidgetRef ref) async {
+    final user = ref.read(userAuthProvider);
+    if (user == null) return;
+
+    final provider = LoginProvider.fromString(user.provider ?? '');
+    await ref.read(userAuthProvider.notifier).logout(provider);
+
+    if (context.mounted) {
+      SignInRoute().go(context);
+    }
   }
 }
