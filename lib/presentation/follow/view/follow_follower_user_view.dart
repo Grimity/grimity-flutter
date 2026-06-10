102f902a2facd1f68e0eb0d99dbfc089562fff8b
Dev Ttangkong <ttankkeo112@gmail.com>
2026-06-10 22:12:36 +0900

    refactor: 잘못된 클래스 이름 수정

--- lib/presentation/follow/view/follow_follower_user_view.dart
+++ lib/presentation/follow/view/follow_follower_user_view.dart
@@ -4,13 +4,13 @@ import 'package:gds/gds.dart';
 import 'package:grimity/app/config/app_router.dart';
 import 'package:grimity/domain/entity/user.dart';
 import 'package:grimity/presentation/common/widget/grimity_infinite_scroll_pagination.dart';
-import 'package:grimity/presentation/common/widget/grimity_state_view.dart';
-import 'package:grimity/presentation/follow/provider/follow_followers_data_provider.dart';
 import 'package:grimity/presentation/follow/widget/follow_user_list_view.dart';
+import 'package:grimity/presentation/common/widget/grimity_state_view.dart';
+import 'package:grimity/presentation/follow/provider/follow_following_data_provider.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:skeletonizer/skeletonizer.dart';
 
-// 팔로워 뷰
+// 팔로잉 뷰
 class FollowerUserView extends HookConsumerWidget {
   const FollowerUserView({super.key});
 
@@ -18,7 +18,7 @@ class FollowerUserView extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     useAutomaticKeepAlive();
 
-    final users = ref.watch(followersDataProvider);
+    final users = ref.watch(followingDataProvider);
 
     return users.when(
       data: (data) {
@@ -33,24 +33,25 @@ class FollowerUserView extends HookConsumerWidget {
             child: GdsEmptyState(
               size: context.isMobile ? GdsEmptyStateSize.md : GdsEmptyStateSize.xl,
               icon: GdsIcon.user,
-              title: '팔로우한 작가가 없어요',
+              title: '빛나는 나를 알아본 사람이 아직 없어요',
               action: GdsSolidButton(
                 size: context.isMobile ? GdsSolidButtonSize.regular : GdsSolidButtonSize.large,
-                text: '인기 그림 둘러보기',
-                onPressed: () => const RankingRoute().go(context),
+                text: '그림 올리기',
+                onPressed: () => const FeedUploadRoute().push(context),
               ),
             ),
           );
         }
 
         return GrimityInfiniteScrollPagination(
-          onLoadMore: ref.read(followersDataProvider.notifier).loadMore,
+          onLoadMore: ref.read(followingDataProvider.notifier).loadMore,
           isEnabled: data.nextCursor != null,
-          child: FollowUserListView(users: users),
+          child: FollowUserListView(users: data.users),
         );
       },
+
       loading: () => Skeletonizer(child: FollowUserListView(users: User.emptyList)),
-      error: (e, s) => GrimityStateView.error(onTap: () => ref.invalidate(followersDataProvider)),
+      error: (e, s) => GrimityStateView.error(onTap: () => ref.invalidate(followingDataProvider)),
     );
   }
 }
--- lib/presentation/follow/view/follow_following_user_view.dart
+++ lib/presentation/follow/view/follow_following_user_view.dart
@@ -4,13 +4,13 @@ import 'package:gds/gds.dart';
 import 'package:grimity/app/config/app_router.dart';
 import 'package:grimity/domain/entity/user.dart';
 import 'package:grimity/presentation/common/widget/grimity_infinite_scroll_pagination.dart';
-import 'package:grimity/presentation/follow/widget/follow_user_list_view.dart';
 import 'package:grimity/presentation/common/widget/grimity_state_view.dart';
-import 'package:grimity/presentation/follow/provider/follow_following_data_provider.dart';
+import 'package:grimity/presentation/follow/provider/follow_followers_data_provider.dart';
+import 'package:grimity/presentation/follow/widget/follow_user_list_view.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:skeletonizer/skeletonizer.dart';
 
-// 팔로잉 뷰
+// 팔로워 뷰
 class FollowingUserView extends HookConsumerWidget {
   const FollowingUserView({super.key});
 
@@ -18,7 +18,7 @@ class FollowingUserView extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     useAutomaticKeepAlive();
 
-    final users = ref.watch(followingDataProvider);
+    final users = ref.watch(followersDataProvider);
 
     return users.when(
       data: (data) {
@@ -33,25 +33,24 @@ class FollowingUserView extends HookConsumerWidget {
             child: GdsEmptyState(
               size: context.isMobile ? GdsEmptyStateSize.md : GdsEmptyStateSize.xl,
               icon: GdsIcon.user,
-              title: '빛나는 나를 알아본 사람이 아직 없어요',
+              title: '팔로우한 작가가 없어요',
               action: GdsSolidButton(
                 size: context.isMobile ? GdsSolidButtonSize.regular : GdsSolidButtonSize.large,
-                text: '그림 올리기',
-                onPressed: () => const FeedUploadRoute().push(context),
+                text: '인기 그림 둘러보기',
+                onPressed: () => const RankingRoute().go(context),
               ),
             ),
           );
         }
 
         return GrimityInfiniteScrollPagination(
-          onLoadMore: ref.read(followingDataProvider.notifier).loadMore,
+          onLoadMore: ref.read(followersDataProvider.notifier).loadMore,
           isEnabled: data.nextCursor != null,
-          child: FollowUserListView(users: data.users),
+          child: FollowUserListView(users: users),
         );
       },
-
       loading: () => Skeletonizer(child: FollowUserListView(users: User.emptyList)),
-      error: (e, s) => GrimityStateView.error(onTap: () => ref.invalidate(followingDataProvider)),
+      error: (e, s) => GrimityStateView.error(onTap: () => ref.invalidate(followersDataProvider)),
     );
   }
 }
